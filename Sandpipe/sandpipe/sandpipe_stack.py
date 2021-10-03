c1baa39ba5ab671e2df0e1e598a678d3ad9f90dc
FarrOut <4e4f9ce1@opayq.com>
2021-10-03 16:38:52 +0200

    added github connection

--- Sandpipe/sandpipe/sandpipe_stack.py
+++ Sandpipe/sandpipe/sandpipe_stack.py
@@ -8,11 +8,17 @@ class SandpipeStack(cdk.Stack):
     def __init__(self, scope: cdk.Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-        # The code that defines your stack goes here
+# CodeStar connection
+# arn:aws:codestar-connections:eu-west-1:595470990922:connection/c8f6dce5-bc13-479d-8015-0c3fe03b68ef
+
+        connection_arn = 'arn:aws:codestar-connections:eu-west-1:595470990922:connection/c8f6dce5-bc13-479d-8015-0c3fe03b68ef'
+
         pipeline =  CodePipeline(self, "Sandpipe",
                         pipeline_name="Sandpipe",
                         synth=ShellStep("Synth",
-                            input=CodePipelineSource.git_hub("FarrOut/InsurgencySandstormDedicatedServer", "main"),
-                            commands=["npm ci", "npm run build", "npx cdk synth"]
+                            input=CodePipelineSource.connection("FarrOut/InsurgencySandstormDedicatedServer", "main",
+                            connection_arn=connection_arn,
+                            ),
+                            commands=["npm install -g aws-cdk", "cdk synth"]
                         )
                     )

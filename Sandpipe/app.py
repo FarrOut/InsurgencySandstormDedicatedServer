495f97d4b0e98e650857abca166e2c9acc676fae
FarrOut <4e4f9ce1@opayq.com>
2021-12-03 14:45:46 +0200

    coorrected classpath to Sandpipestack

--- Sandpipe/app.py
+++ Sandpipe/app.py
@@ -10,7 +10,7 @@ from aws_cdk import core
 
 # Environments
 from sandpipe.gameserver_stack import GameserverStack
-from Sandpipe.sandpipe.sandpipe_stack import SandpipeStack
+from sandpipe.sandpipe_stack import SandpipeStack
 
 env_cpt = core.Environment(account=os.getenv('CDK_DEFAULT_ACCOUNT'), region='af-south-1')
 
